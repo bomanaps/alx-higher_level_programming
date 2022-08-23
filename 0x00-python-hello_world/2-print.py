294bb3fa1ea920d66777ad365785757678bb9b62
bomanaps <mercy.boma35@yahoo.com>
2022-08-23 00:38:20 +0000

    python task 3

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-print('"programming is like building a multilingual puzzle")#!/usr/bin/python3
-print('"programming is like building a multilingual puzzle")
+print("\"programming is like building a multilingual puzzle")

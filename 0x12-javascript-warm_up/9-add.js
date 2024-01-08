1d5014a858645aa35caf2d1cafd53138bfd8b1a2
bomanaps <mercy.boma35@yahoo.com>
2024-01-08 06:18:54 -0800

    fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,6 +2,6 @@
 function add (a, b) {
   return a + b;
 }
-
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
-
+const val1 = parseInt(process.argv[2]);
+const val2 = parseInt(process.argv[3]);
+console.log(add(val1, val2));

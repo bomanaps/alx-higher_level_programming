0cf1a798f7858391fd01c55bbb5374b43697fb77
bomanaps <mercy.boma35@yahoo.com>
2024-01-08 13:26:57 -0800

    fix

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 exports.addMeMaybe = function (number, theFunction) {
-  theFunction(++number);
+  number += 1;
+  theFunction(number);
 };
-
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,24 @@
 #!/usr/bin/node
+let val;
+let val2;
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const args = process.argv.map(Number)
-    .slice(2, process.argv.length)
-    .sort((a, b) => a - b);
-  console.log(args[args.length - 2]);
+  val = parseInt(process.argv[2]);
+  for (let i = 2; i < process.argv.length; i++) {
+    if (parseInt(process.argv[i]) > val) {
+      val = parseInt(process.argv[i]);
+    }
+  }
+  if (val !== parseInt(process.argv[2])) {
+    val2 = parseInt(process.argv[2]);
+  } else {
+    val2 = parseInt(process.argv[3]);
+  }
+  for (let i = 2; i < process.argv.length; i++) {
+    if (parseInt(process.argv[i]) > val2 && parseInt(process.argv[i]) !== val) {
+      val2 = parseInt(process.argv[i]);
+    }
+  }
+  console.log(val2);
 }
-

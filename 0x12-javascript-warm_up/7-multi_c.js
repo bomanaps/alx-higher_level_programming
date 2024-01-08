8b3b00cb67ac15855d9461904fbc6f2beb328cb6
bomanaps <mercy.boma35@yahoo.com>
2024-01-08 06:15:00 -0800

    fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
-  console.log('Missing number of occurrences');
-} else {
+let x;
+if (parseInt(process.argv[2])) {
+  x = parseInt(process.argv[2]);
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
+} else {
+  console.log('Missing number of occurrences');
 }
-

0411047eacc399f2aad2d82df5506accfa880fa9
bomanaps <mercy.boma35@yahoo.com>
2024-01-26 14:45:12 -0800

    fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Use requests package to make a get request to given URL
-"""
+""" script that fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
+
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    url = 'https://alx-intranet.hbtn.io/status'
+    res = requests.get(url)
+    print('Body response:')
+    print('\t-', 'type:', type(res.text))
+    print('\t-', 'content:', res.text)

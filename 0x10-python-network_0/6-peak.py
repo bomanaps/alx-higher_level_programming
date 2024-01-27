20aa7e7ebd5970e54d068f8dd5731fd94ce5b268
bomanaps <mercy.boma35@yahoo.com>
2024-01-27 12:13:14 -0800

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""task 6 modele"""
+""" find peak number """
 
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+    """ find_peak(list_of_integers): to find peak
+        Args:
+            list_of_integers: list passed
+        Return: peak
+    """
+    if list_of_integers is None or len(list_of_integers) == 0:
         return None
-
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
-
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    lst = list_of_integers
+    for i in range(0, len(lst)):
+        if i == 0:
+            if lst[i] > lst[i + 1]:
+                peak = lst[i]
+                return peak
+        elif i != 0 and i != len(lst) - 1:
+            j = i - 1
+            y = i + 1
+            if lst[i] > lst[j] and lst[i] > lst[y]:
+                peak = lst[i]
+                return peak
+        elif i == len(lst) - 1:
+            if lst[i] > lst[i - 1]:
+                peak = lst[i]
+                return peak
+    return lst[0]

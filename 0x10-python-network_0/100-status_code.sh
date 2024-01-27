c17f14213305d6aaef211365938717635eff0405
bomanaps <mercy.boma35@yahoo.com>
2024-01-27 12:05:57 -0800

    fix

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
- #!/bin/bash
-# sends a req to $1 URL, display response status code only
-curl -s -o /dev/null -w "%{http_code}" "$1"
+#!/bin/bash
+#script that sends a request to a URL passed as an argument, and displays only the status code of the response
+curl -s -o /dev/null  "$1" -w '%{http_code}'
